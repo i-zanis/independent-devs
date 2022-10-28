a0c61e8da7fb5911e859ad6199060018672fa562
i-zanis <ioanlafazanis@gmail.com>
2022-10-28 23:31:32 +0100

    initial commit

---
+++ src/main/java/com/example/independentdevs/IndependentDevsApplication.java
@@ -0,0 +1,13 @@
+package com.example.independentdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndependentDevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndependentDevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/independentdevs/IndependentDevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.independentdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndependentDevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
